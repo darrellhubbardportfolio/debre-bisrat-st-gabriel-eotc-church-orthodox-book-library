60eac0ea63ad4e179957f1a53799537757cdbede
Darrell Hubbard <thehubbard89@gmail.com>
2025-03-11 12:22:11 -0400

    fix the donate book page

--- index.js
+++ index.js
@@ -25,41 +25,46 @@ app.use(session({
     resave: false
 }));
 
+// load models
+const { Books } = require("./models/BookModel");
+const books = new Books(); // create a new instance
+
 // home page
 app.get("/", (req, res) => {
     // res.send("home page. this is the library, first page users will see.");
     res.render("index");
 }); // this is like the library.
 
-// book
-app.get("/book", (req, res) => {
-    res.send("book page. view a particular book");
-});
-
-// read a book
-app.get("/book/book", (req, res) => {
-    res.send("read book on this page.");
-});
-
-const { Books } = require("./models/models");
 // this route, use a fetch request to get all books
-app.get("/api/books", 
+app.get("/api/library", 
     async (req, res) => {
 
-    const Book = new Books();
-    let book = await Book.read();
+    let book = await books.read();
     res.json(book);
 });
 
+// book (find a specific book)
+app.get("/book/:title", 
+    async (req, res) => {
+        const { title } = req.params;
+        let book = await books.findBook(title);
+        res.render("book", { book });
+});
+
 // church members upload pdf file 
-app.get("/pdf-upload-request", (req, res) => {
+app.get("/donate-a-book", (req, res) => {
     // res.send("pdf upload form. church members can upload a new pdf file.");
-    res.render("request-pdf-upload");
+    res.render("book-donation-page");
 })
 
 // church members will upload a new file here for download.
-app.post("/pdf-upload-request/post", (req, res) => {
-    res.send("you have donated a new book to the library.");
+app.post("/donate-a-book/post", (req, res) => {
+
+    const { name, email } = req.body;
+    const book = req.file;
+    
+    // save to database
+    res.json({name: name, email: email});
 });
 
 // administrator can view all categories here
